58ce988583a710c5f11889c03976bfcbbe84b0c6
arisath <arist.athanas@gmail.com>
2020-03-26 22:31:43 +0000

    Adding subscriber

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
 import com.example.model.Message;
-import com.example.service.Service;
+import com.example.service.PublisherService;
+import com.example.service.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @Autowired
+    private PublisherService publisherService;
+
 	@Autowired
-	private Service service;
+	private SubscriberService subscriberService;
 
-	@PostMapping("/publish")
-	public String publish(@RequestBody Message message) throws Exception
-	{
-		String[] ar = {"first-topic",message.getMessage()};
+    @PostMapping("/publish")
+    public String publish(@RequestBody Message message) throws Exception {
+        publisherService.publish(message.getMessage());
 
-		service.publish(ar);
+        return "Message submitted to Pub/Sub";
+    }
 
-		return "Message submitted to Pub/Sub";
-	}
+    @GetMapping("/receive")
+    public String receive() throws Exception {
+		subscriberService.receive();
 
-
+		return "Messages received";
+    }
 
 
 }
--- src/main/java/com/example/service/PublisherService.java
+++ src/main/java/com/example/service/PublisherService.java
@@ -7,19 +7,23 @@ import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @org.springframework.stereotype.Service
-public class Service {
+public class PublisherService {
 
     private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
 
-    public void publish(String[] args) throws Exception {
+    @Value("${topic}")
+    private String topic;
 
-    String topicId = args[0];
+    public void publish(String message) throws Exception {
+
+    String topicId = topic;
     ProjectTopicName topicName = ProjectTopicName.of(PROJECT_ID, topicId);
     Publisher publisher = null;
     List<ApiFuture<String>> futures = new ArrayList<>();
@@ -28,20 +32,15 @@ public class Service {
 
             publisher = Publisher.newBuilder(topicName).build();
 
-            String message = args[1];
-
-            // convert message to bytes
             ByteString data = ByteString.copyFromUtf8(message);
             PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
                     .setData(data)
                     .build();
 
-            // Schedule a message to be published. Messages are automatically batched.
             ApiFuture<String> future = publisher.publish(pubsubMessage);
             futures.add(future);
 
         } finally {
-            // Wait on any pending requests
             List<String> messageIds = ApiFutures.allAsList(futures).get();
 
             for (String messageId : messageIds) {
@@ -49,7 +48,6 @@ public class Service {
             }
 
             if (publisher != null) {
-                // When finished with the publisher, shutdown to free up resources.
                 publisher.shutdown();
             }
         }
