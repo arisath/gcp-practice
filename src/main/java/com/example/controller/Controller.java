1fa3f640185a77c4bfeb0042b90be9623dc663ff
arisath <arist.athanas@gmail.com>
2020-04-03 00:32:27 +0100

    Formatting

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,16 +1,12 @@
 package com.example.controller;
 
 import com.example.model.Message;
-import com.example.model.ReceivedMsg;
 import com.example.service.AsynchronousSubscriberService;
 import com.example.service.PublisherService;
-import com.example.service.AsynchronousSubscriberService;
 import com.google.gson.Gson;
-import com.google.pubsub.v1.ReceivedMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class Controller
